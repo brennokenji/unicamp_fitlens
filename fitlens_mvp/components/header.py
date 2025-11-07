3d59e525614800a4968b6a354d5577d4ebce4c0c
brennokenji <brenno.jaime@mmccreation.com>
2025-11-07 17:14:09 -0300

    fix: corrigindo erro de portugues

--- fitlens_mvp/components/header.py
+++ fitlens_mvp/components/header.py
@@ -3,7 +3,7 @@ import streamlit as st
 def show_header():
     st.title('🏋️‍♀️ FitLens')
     st.subheader('Análise em tempo real de movimentos -- powered by Gabrena')
-    if st.button('Treinando sozinho? Uilize o FitStand!'):
+    if st.button('Treinando sozinho? Utilize o FitStand!'):
         # st.warning('💲 O seu 1º FitStand é gratuito! 💲')
         st.toast('💲 O seu 1º FitStand é gratuito!')
     st.markdown('---')
